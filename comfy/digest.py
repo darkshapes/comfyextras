feb355798a90cb9e0396854a56e8160851a5ae53
Benjamin Berman <benjamin.s.berman@gmail.com>
2023-08-22 18:00:18 -0700

    move this back since the issue is resolved

--- comfy/digest.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-import json
-
-
-def digest(data: dict | str) -> str:
-    json_str = data if isinstance(data, str) else json.dumps(data, separators=(',', ':'))
-    hash_object = hashlib.sha256()
-    hash_object.update(json_str.encode())
-    return hash_object.hexdigest()
--- main.py
+++
@@ -1,4 +0,0 @@
-from comfy.cmd.main import main
-
-if __name__ == "__main__":
-    main()
